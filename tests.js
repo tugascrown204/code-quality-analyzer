29ee3ee58261d4d59c712adca07049cc17df9e90
tugascrown204 <gajdertevet@hotmail.com>
2025-09-13 18:33:26 +0330

    Ensure the test case checks the file content after reading.

--- tests.js
+++ tests.js
@@ -3,4 +3,5 @@ const { readFileAsync } = require('./utils');
 test('should correctly read the contents of a file using readFileAsync', async () => {
   const data = await readFileAsync('sample.txt');
   expect(data).toBeDefined();
+  expect(data).toMatch(/expected content/); // Add an expectation to verify content
 });
