0533166c0227b55846b5c5b260298f5e459d1b3c
tugascrown204 <gajdertevet@hotmail.com>
2025-09-13 18:33:09 +0330

    Fix: Add missing return statement on readFileAsync promise reject

--- utils.js
+++ utils.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const readFileAsync = (filePath) => {
   return new Promise((resolve, reject) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) reject(err);
+      if (err) return reject(err);
       else resolve(data);
     });
   });
