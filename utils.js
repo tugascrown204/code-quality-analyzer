716f7dd83f44c1a22c99bb1ff84b38d3b7981e7a
tugascrown204 <gajdertevet@hotmail.com>
2025-09-13 18:33:40 +0330

    Refactor readFileAsync to use util.promisify for cleaner code

--- utils.js
+++ utils.js
@@ -1,12 +1,6 @@
 const fs = require('fs');
+const { promisify } = require('util');
 
-const readFileAsync = (filePath) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err) return reject(err);
-      else resolve(data);
-    });
-  });
-};
+const readFileAsync = promisify(fs.readFile);
 
 module.exports = { readFileAsync };
